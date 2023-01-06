f8066b32b110dbe0156ac3141fdd2efb90f904f4
iko4akov <iko4akov@gmai.com>
2023-01-06 19:58:34 +0300

    change view.py

--- bp_main/view.py
+++ bp_main/view.py
@@ -4,6 +4,11 @@ from bp_main.dao.main_dao import get_count_comment
 
 from bp_main.dao.main_dao import PostsDAO
 
+
+POSTS_PATH = 'data/posts.json'
+
+COMMENT_PATH = 'data/comments.json'
+
 # Создаем блюпринт главной страницы
 main_blueprint = Blueprint("main_blueprint", __name__, template_folder='templates')
 
@@ -19,9 +24,9 @@ post_blueprint = Blueprint("post_blueprint", __name__, template_folder='template
 
 @main_blueprint.get("/")
 def index_page():
-    post_handler = PostsDAO('data/posts.json')
+    post_handler = PostsDAO(POSTS_PATH)
     posts = post_handler.load_file()
-    comment_handler = PostsDAO('data/comments.json')
+    comment_handler = PostsDAO(COMMENT_PATH)
     comments = comment_handler.load_file()
     com_post = get_count_comment(posts, comments)
     return render_template("index.html", posts=posts, comments=comments, com_post=com_post)
@@ -33,25 +38,25 @@ def search_page():
     # Условие для отображения пустой страницы поиска
     if substr is None:
         substr = "anti_error_search_page"
-        post_handler = PostsDAO('data/posts.json')
+        post_handler = PostsDAO(POSTS_PATH)
         fined_posts = post_handler.search_posts(substr)
     else:
-        post_handler = PostsDAO('data/posts.json')
+        post_handler = PostsDAO(POSTS_PATH)
         fined_posts = post_handler.search_posts(substr)
     return render_template("search.html", fined_posts=fined_posts)
 
 
 @user_blueprint.get('/user/<user_name>')
 def user_page(user_name):
-    post_handler = PostsDAO('data/posts.json')
+    post_handler = PostsDAO(POSTS_PATH)
     posts_for_name = post_handler.get_posts_by_user(user_name)
     return render_template("user-feed.html", posts_for_name=posts_for_name, user_name=user_name)
 
 
 @post_blueprint.get('/post/<int:pk>')
 def post_page(pk):
-    comment_handler = PostsDAO('data/comments.json')
-    post_handler = PostsDAO('data/posts.json')
+    comment_handler = PostsDAO(COMMENT_PATH)
+    post_handler = PostsDAO(POSTS_PATH)
     post = post_handler.get_post_by_pk(pk)
     comments = comment_handler.get_comments_by_post_id(pk)
     return render_template("post.html", post=post, comments=comments)
